acb277a8665117f98966eca2da49f587934faca0
Omar Sami <omarsami47@gmail.com>
2022-12-09 21:22:08 +0000

    Fix2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,15 +5,16 @@
  *
  * Return: 0 if success
  */
+
 int main(void)
 {
-  char c = 'a'
+	char c = 'a';
 
-    while (c <= 'z')
-      {
-	putchar(c);
-	c++;
-      }
-  putchar('\n');
-  return (0);
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
 }
