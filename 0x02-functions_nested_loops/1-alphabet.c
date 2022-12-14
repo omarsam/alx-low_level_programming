f8de66ab7c826eab65d9006943925c8d8a16e216
Omar Sami <omarsami47@gmail.com>
2022-12-14 17:57:19 +0000

    Adding 1-alphabet.c and updating main.h

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet, in lowercase, followed by a new line
+ * created by Omar El-Shazly
+ * Return: 0 When success
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
+	return (0);
+}
