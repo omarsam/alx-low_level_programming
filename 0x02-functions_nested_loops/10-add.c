fd27ae84dd7b7c15fdf93686a37011841da8d6e9
Omar Sami <omarsami47@gmail.com>
2022-12-14 18:57:30 +0000

    Adding 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - dds two integers and returns the result
+ * @i: first number
+ * @n: second number
+ *
+ * Return: result
+ *
+ * Creeated by Omar El-Shazly
+ */
+
+int add(int i, int n)
+{
+	return (i + n);
+}
