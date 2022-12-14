40acde2970fad1bf58813629d86c6b867e06650d
Omar Sami <omarsami47@gmail.com>
2022-12-14 17:20:40 +0000

    Creating header file and putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - print _putchar
+ * written by omar el-shazly
+ * Return: 0 When success
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	retunr (0);
+}
