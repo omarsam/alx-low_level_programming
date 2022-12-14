0b4770e29ab6a238e82f4508130fd8f797ceb2d7
Omar Sami <omarsami47@gmail.com>
2022-12-14 18:34:04 +0000

    Adding 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @int: value of testing
+ *
+ * Return: Always 0
+ *
+ * Created by Omar El-Shazly
+ */
+
+int _abs(int)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
