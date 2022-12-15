cd0ad7fb7cfca8b6af4cd5639741afd193729158
Omar Sami <omarsami47@gmail.com>
2022-12-15 08:43:56 +0000

    Adding 5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * more_numbers -  prints 10 times the numbers, from 0 to 14
+ * followed by a new line
+ *
+ * Created by Omar El-Shazly
+ */
+
+void more_numbers(void)
+{
+	int number, tens, units, row;
+
+	for (row = 1; row <= 10; row++)
+	{
+		for (number = 0; number <= 14; number++)
+		{
+			tens = number / 10;
+			units = number % 10;
+			if (number > 9)
+				_putchar(tens + '0');
+
+			_putchar(units + '0');
+		}
+		_putchar('\n');
+	}
+}
