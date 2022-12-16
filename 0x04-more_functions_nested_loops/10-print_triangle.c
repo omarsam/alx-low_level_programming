ca247e376b14890a7ac6aca608a3765d7e32db54
Omar Sami <omarsami47@gmail.com>
2022-12-16 02:04:03 +0000

    Trying to fix

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -22,7 +22,7 @@ void print_triangle(int size)
 		{
 			for (spaces = size - row; spaces >= 1; spaces--)
 			{
-				_pucthar(' ');
+				_putchar(' ');
 			}
 			for (hashes = 1; hashes <= row; hashes++)
 			{
