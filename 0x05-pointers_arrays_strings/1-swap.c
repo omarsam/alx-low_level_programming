c04bed46efe02470e58bb2c604da8adb713f393a
Omar Sami <omarsami47@gmail.com>
2022-12-19 08:53:32 +0000

    Adding 1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two integers
+ *
+ * @a: first integer
+ * @b: second integer
+ *
+ * Created by Omar El-Shazly
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c = *a;
+
+	*a = *b;
+	*b = *c;
+}
