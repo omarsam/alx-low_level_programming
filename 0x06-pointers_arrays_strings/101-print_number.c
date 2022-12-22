9b77fc83755450238688e93979a18d258499a9c8
Omar Sami <omarsami47@gmail.com>
2022-12-22 13:45:26 +0000

    a function that prints an integer.

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,24 @@
+#inlcude "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: integer to be printed
+ *
+ * Created by Omar El-Shazly
+ */
+
+void print_number(int n)
+{
+	unsigned int i = n;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		i = -i;
+	}
+	if (i / 10)
+	{
+		print_number(i / 10);
+	}
+	_putchar(i % 10 + '0');
+}
