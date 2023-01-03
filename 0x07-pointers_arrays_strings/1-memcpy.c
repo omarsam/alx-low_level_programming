203a24185fb2a1336d605834379a31af9a77f2c4
Omar Sami <omarsami47@gmail.com>
2023-01-03 18:15:12 +0000

    uploading strchr

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _memcpy - function that copies memory area
+ * @s: array
+ * @c: char
+ *
+ * Return: char
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] != 0)
+	{
+		if (s[i] == c)
+			return (&s[i]);
+		i++;
+	}
+	if (s[i] == c)
+		return (&s[i]);
+	return ('\0');
+}
