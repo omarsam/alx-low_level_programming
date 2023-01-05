2262f8f5b2a00655056f767dbb6f48b3c9606109
Omar Sami <omarsami47@gmail.com>
2023-01-05 14:48:37 +0000

    Adding factorial.c

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * factorial - function that returns the factorial of a given number
+ * @n: the integer
+ * Return: the factorial of given number
+ *
+ * Created by Omar El-Shazly
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
