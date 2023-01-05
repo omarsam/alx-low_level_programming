4c91370ea351333cbe7b8f15f8da97bc6523edf9
Omar Sami <omarsami47@gmail.com>
2023-01-05 15:01:35 +0000

    Adding 100-wildcmp.c

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+*wildcmp - compare strings
+*@s1: string
+*@s2: string
+*
+*Return: 1 -> identical, else  -> 0
+*/
+int wildcmp(char *s1, char *s2)
+{
+	if (!*s1 && !*s2)
+	{
+		return (1);
+	}
+	if ((*s2 == '*') && !*s1 && (*(s2 + 1) != '\0'))
+	{
+		return (0);
+	}
+	if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+	}
+	return (0);
+}
