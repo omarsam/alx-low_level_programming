02ccc919fc5083ec9390de844f8e24ecbec1ed49
Omar Sami <omarsami47@gmail.com>
2023-01-10 10:11:40 +0000

    Adding README.md, main.h, and _pucthar.c

---
+++ 0x0B-malloc_free/_putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The char to print
+ *
+ * Return: 1 on success
+ * On error, -1 is returned and errno is set appropriately
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
