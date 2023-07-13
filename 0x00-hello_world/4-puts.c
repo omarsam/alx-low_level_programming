bea498e75d1e1aefac9afd16e88ad20e3a04a032
omarsam <omarsami47@gmail.com>
2023-07-13 03:01:07 -0700

    Adding 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Return: 0 when success
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
